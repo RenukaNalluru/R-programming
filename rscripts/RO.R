355e71a29f081c33705fd8d23710385fbafcec88
unknown <christoffer@medcast01.vpn.incor.org>
2015-03-12 18:57:29 -0300

    adicionando biblioteca ROracle

--- rscripts/RO.R
+++ rscripts/RO.R
@@ -1,3 +1,7 @@
+# Introducing Oracle R Enterprise
+# https://docs.oracle.com/cd/E57012_01/doc.141/e56973/intro.htm#OREUG187
+
+
 library(OREbase)
 library(OREcommon)
 library(OREembed)
@@ -18,10 +22,14 @@ OREbase::factorial(x = 100)
 
 ore.connect(user = "hr", sid = "xe", host = "localhost", password = "admin", port = 1521)
 
+?OREbase::ore.exe    
+object <- print(OREbase::ore.exec(qry = "SELECT * FROM SI3_FONETIC"))
     
+ore.exists(name = "SI3_FONETIC")
     
-    
-    
-    
-    
-    
+OREbase::ore.create(x = data.frame(c(1:10), row.names = c(1:10)), table = "ANY_TABLE")
+OREbase::ore.drop(table = "ANY_TABLE")
+
+?Oracle   # package ROracle
+?dbDriver # package DBI
+dbDriver("Oracle")
