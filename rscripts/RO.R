12bb5399cb3803a0972dce7370740d91ce4852e6
unknown <christoffer@medcast01.vpn.incor.org>
2015-03-16 18:54:07 -0300

    Estudo de performance Oracle e R

--- rscripts/RO.R
+++ rscripts/RO.R
@@ -21,22 +21,53 @@ OREbase::factorial(x = 100)
 ?ore.connect
 
 ore.connect(user = "hr", sid = "xe", host = "localhost", password = "admin", port = 1521)
+OREbase::ore.is.connected()
+?OREbase::ore.connect
+?OREbase::ore.exec
 
-?OREbase::ore.exe    
-object <- print(OREbase::ore.exec(qry = "SELECT * FROM SI3_FONETIC"))
+OREbase::ore.exec(qry = "SELECT * FROM SI3_FONETIC")
+# object <- print(OREbase::ore.exec(qry = "SELECT * FROM SI3_FONETIC"))
     
-ore.exists(name = "SI3_FONETIC")
-    
-OREbase::ore.create(x = data.frame(c(1:10), row.names = c(1:10)), table = "ANY_TABLE")
+OREbase::ore.create(x = data.frame(x = c(1:10), row.names = c(1:10)), table = "ANY_TABLE")
+ore.get("ANY_TABLE")
+ore.exists("ANY_TABLE")
 OREbase::ore.drop(table = "ANY_TABLE")
+
+
+?interactive
+?OREbase::ore.get
+
+OREbase::ore.disconnect()
+
+
 
 ?Oracle   # package ROracle
 ?dbDriver # package DBI
-dbDriver("Oracle")
+drive <- dbDriver("Oracle")
+conn <- dbConnect(drv = drive, "hr", "admin")
+table <- dbReadTable(conn, name = "SI3_FONETIC")
 
+setwd(dir = "C:/Users/christoffer/Desktop/R-programming/")
+f <- paste(getwd(), "/rscripts/RecordLinkageStudy/UtilsRecordLinkage.R", sep = "")
+t <- file.exists(f)
+ifelse(test = t, yes = source(file = f), no = q())
 
+unix.time(expr = sapply(1:ncol(table), function(i) nrow(table[is.na(table[, i]), ])))
+# user  system elapsed 
+# 1.25    0.05    1.30
 
+unix.time(table.notna <- listNotNa(X = table, fields = 1))
+# user  system elapsed 
+# 1.66    0.03    1.70 
+
+unix.time(table.notna <- clear.all.matrix(data = table.notna, fields = 2:ncol(table.notna)))
+# user  system elapsed 
+# 5.14    0.18    5.36
+
+# demo(package = "ORE")
 # teste com rJava
+
+dbDisconnect(conn = conn);
 
 .jinit()
 vect <- .jnew("java/util/Vector")
--- rscripts/RecordLinkageStudy/CachingV3-SI3.R
+++ rscripts/RecordLinkageStudy/CachingV3-SI3.R
@@ -1,5 +1,6 @@
 library(ipred)
 library(RecordLinkage)
+library(RSQLite)
 library(ff)
 library(RJDBC)
 library(SOAR)
@@ -20,15 +21,14 @@ library(R.cache)
 
 nrow(si3.fonetic) #1177129
 
-
 f <- paste(getwd(), "/rscripts/RecordLinkageStudy/UtilsRecordLinkage.R", sep = "")
 t <- file.exists(f)
 ifelse(test = t, yes = source(file = f), no = q())
 
-
-
-sapply(1:ncol(si3.fonetic), function(i) nrow(si3.fonetic[is.na(si3.fonetic[, i]), ]))
+unix.time(sapply(1:ncol(si3.fonetic), function(i) nrow(si3.fonetic[is.na(si3.fonetic[, i]), ])))
 # 0  22145      0      0 183779
+# user  system elapsed 
+# 1.46    0.06    1.51
 
 unix.time(si3.fonetic.notna <- listNotNa(X = si3.fonetic, fields = 1))
 # usuario   sistema decorrido 
@@ -138,15 +138,146 @@ test.psi3 <- RecordLinkage::compare.dedup(dataset = test.duplicated, blockfld = 
 test.psi3$pairs[test.psi3$pairs[, 8] == 1]
 nrow(subset(test.psi3$pairs, (PACB_TP_SEXO > 0.89) & (PACI_DT_NASC > 0.89) & (PACB_CF_NM_COM > 0.89)))
 
-
+library(RecordLinkage)
 data(RLdata10000)
+data(RLdata500)
+RLdata10000[1,]
+RLdata10000[RLdata10000$fname_c1 == "FRANK" & RLdata10000$lname_c1 == "SCHROEDER",]
+ 
+# http://www.cookbook-r.com/Manipulating_data/Finding_and_removing_duplicate_records/
+# stackoverflow.com/questions/10769640/how-to-remove-repeated-elements-in-a-vector
+sorted <- unique(sort(identity.RLdata10000, decreasing = F))
+# sorted[!sorted %in% c(0)][1:100]
 
-# limite 
-rl1 <- RecordLinkage::compare.dedup(dataset = RLdata10000, blockfld =  list(1,3,5),  strcmp = T, identity = identity.RLdata10000)
+cbind(RLdata10000[1:100, c(1,3,5)], RLdata10000[sorted[!sorted %in% c(0)][1:100], c(1,3,5)])
 
+
+# limite
+# ?RecordLinkage::compare.dedup
+# range <- 1:length(sorted[!sorted %in% c(0)])
+# rl1 <- RecordLinkage::compare.dedup(dataset = RLdata10000[range, ], blockfld =  list(1,3,5),  strcmp = T, identity = range)
+
+#{1,3,5,6,7}
+
+unix.time(rl1<-RecordLinkage::compare.dedup(dataset = RLdata10000, blockfld =  list(1, 3, 5, 7),  strcmp = T, identity = identity.RLdata10000))
+# tempo gasto com os campos (1, 5, 7)
+# usuário   sistema decorrido 
+# 72.94      1.26     74.86 
+
+unix.time(rl5<-RecordLinkage::compare.dedup(dataset = RLdata500, blockfld =  list(1, 3, 5, 7),  strcmp = T, identity = identity.RLdata500))
+gc(verbose = T)
+rm(list=ls(all.names = T))
+gcinfo(TRUE)
+nrow(rl1$pairs[rl1$pairs$is_match == 1,  ]) # {1:658; (1, 3):992; (1,3,5):997; (1,5,7):999, }
+nrow(rl5$pairs[rl5$pairs$is_match == 1,  ]) # {1:34; (1, 3):49, (1,3, 5, 7):50}
+
+
+unix.time(ans<-RecordLinkage::RLBigDataDedup(dataset = RLdata10000, identity = identity.RLdata10000, blockfld = list(1,5,7), strcmp = T))
+# tempo gasto com os campos (1, 5, 7)
+# usuário   sistema decorrido 
+# 16.69      0.69     22.80
+# ?epiWeights
+# class(ans)
+# ?RecordLinkage::getFrequencies(x = ans)
+
+unix.time(ans <- RecordLinkage::epiWeights(rpairs = ans, f = RecordLinkage::getFrequencies(ans)))
+# usuário   sistema decorrido 
+# 4.74      0.39      5.14
+# class(ans)
+# ?epiClassify
+
+################## Teste 1 com epiClassify e getPairs
+
+unix.time(rs <- RecordLinkage::epiClassify(rpairs = ans, threshold.upper =  0.5))
+class(rs)
+unix.time(table <- RecordLinkage::getTable(rs))
+# usuário   sistema decorrido 
+# 4.45      0.15      4.61
+class(table)
+?RecordLinkage::getPairs
+
+## S4 method for signature 'RLResult'
+unix.time(pairs <- RecordLinkage::getPairs(object = rs, max.weight = 0.8, filter.link="link"))
+# usuário   sistema decorrido 
+# 0.46      0.06      0.52 
+# class(pairs)
+# nrow(pairs) # {
+#     com "threshold.upper" do metodo "RecordLinkage::epiClassify" em 0.5
+#     com "max.weight" em 0.8
+#     foram gerados 590958 pares
+#}
+
+pairs[!pairs[1:100, ]$is_match == "", ]
+unix.time(RecordLinkage::getFalse(object = rs))
+RecordLinkage::getFalseNeg(object = rs)
+RecordLinkage::getFalsePos(object = rs)
+
+################### fim Teste 1
+
+################### Teste 2 com epiClassify e getPairs
+
+unix.time(rs <- RecordLinkage::epiClassify(rpairs = ans, threshold.upper =  0.7))
+unix.time(pairs <- RecordLinkage::getPairs(object = rs, min.weight = 0.5, max.weight = 0.8, filter.link = "link"))
+# usuário   sistema decorrido 
+# 0.25      0.06      0.31
+# nrow(pairs) # {
+#     com "threshold.upper" do metodo "RecordLinkage::epiClassify" em 0.7
+#     com "max.weight" em 0.8 min.weight = 0.5
+#     foram gerados 2763 pares
+# com os mesmos parametros na funcao epiClassify porem mudando o argumento
+# filter.match = "match" filter.link = "link" encontrei 2091 pares
+
+#}
+
+
+
+?RecordLinkage::getFalse
+unix.time(RecordLinkage::getFalse(object = rs))
+RecordLinkage::getFalseNeg(object = rs)
+RecordLinkage::getFalsePos(object = rs)
+
+
+################### fim Teste 2
+
+################### Teste 3 com epiClassify e getPairs
+
+unix.time(rs <- RecordLinkage::epiClassify(rpairs = ans, threshold.upper =  0.8))
+unix.time(pairs <- RecordLinkage::getPairs(object = rs, min.weight = 0.7, max.weight = 0.8, filter.match = "match"))
+# usuário   sistema decorrido 
+# 0.25      0.06      0.31
+# nrow(pairs) # {
+#     com "threshold.upper" do metodo "RecordLinkage::epiClassify" em 0.8
+#     com "max.weight" em 0.8 min.weight = 0.7
+#     foram gerados 2007 pares
+
+#     com "max.weight" em 0.8 min.weight = 0.7
+#     com "max.weight" em 0.9 min.weight = 0.7
+#       2238
+# threshold.upper =  0.8 min.weight = 0.8, max.weight = 0.9
+# 231
+#     
+#}
+
+
+
+?RecordLinkage::getFalse
+unix.time(RecordLinkage::getFalse(object = rs))
+RecordLinkage::getFalseNeg(object = rs)
+RecordLinkage::getFalsePos(object = rs)
+
+
+################### fim Teste 3
+
+
+
+
+# RecordLinkage::getPairs(object = rs) nao exeuctar somente com o parametro object
+?RecordLinkage::clear
+
+summary(ans)
+# levenshteinDist("AXAX", "CBEEE")
 # diferenca de pares resultantes conforme a quantidade de campos no blockfld
 rl1.pairs <- rl1$pairs # { {1:472774}, {(1,3):1320853}, {(1,3,5):1929865} }
-
 # cn  <- colnames(x = test.psi3$pairs[, 4:ncol(test.psi3$pairs)-1])
 # remove aspas
 # cat(cn)
